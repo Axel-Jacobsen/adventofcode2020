552f8a902e8c2986ffed22ba6a8be6e58a2bd4f5
Axel-Jacobsen <axelnj44@gmail.com>
2020-12-06 14:57:33 -0800

    d5 optimization

--- day5/d5.py
+++ day5/d5.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+import timeit
 
 
 def get_row_col(seat_bsp: str) -> int:
@@ -6,13 +7,18 @@ def get_row_col(seat_bsp: str) -> int:
     col_bsp_ref = seat_bsp[7:].replace("L", "0").replace("R", "1")
     return int(row_bsp_ref, 2), int(col_bsp_ref, 2)
 
+def f():
+    with open("input.txt", "r") as f:
+        seat_ids = sorted(
+            8 * get_row_col(line.strip())[0] + get_row_col(line.strip())[1] for line in f.readlines()
+        )
+        p1 = seat_ids[-1]
+        for s1, s2 in zip(seat_ids, seat_ids[1:]):
+            if s2 - s1 == 2:
+                p2 = s1 + 1
+                break
+    return p1, p2
 
-with open("input.txt", "r") as f:
-    seat_ids = sorted(
-        8 * get_row_col(line.strip())[0] + get_row_col(line.strip())[1] for line in f.readlines()
-    )
-    print("part 1", seat_ids[-1])
-    for s1, s2 in zip(seat_ids, seat_ids[1:]):
-        if s2 - s1 == 2:
-            print("part 2", s1 + 1)
-            break
+p1, p2 = f()
+print("part 1", p1)
+print("part 2", p2)
