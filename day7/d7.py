0ef2c8ee82c73427be863faeb599bff1b35d3387
Axel-Jacobsen <axelnj44@gmail.com>
2020-12-07 16:26:22 -0800

    fin d7

--- day7/d7.py
+++ day7/d7.py
@@ -1,42 +1,88 @@
 #! /usr/bin/env python3
 
-# < 322
-
-# swear makes my developing faster
 from typing import Dict, List, Set
 
 
-Bag = str
+def part1():
+    Bag = str
+    with open("input.txt", "r") as f:
+
+        bag_held_in: Dict[Bag, List[Bag]] = dict()
+
+        for line in f.readlines():
+            # remove "formatting" words
+            line = (
+                line.replace("no other bags", "")
+                .replace(" bags", "")
+                .replace(" bag", "")
+            )
+
+            # split into parent and child bags
+            super_bag, b = line.split(" contain ")
+            bag_colors_it_holds = [
+                g[2:] for g in b.replace(".\n", "").split(", ") if g != "no other bags"
+            ]  # first 2 chars are num and a space
+
+            # set map of bag to the bags that hold it
+            for bag in bag_colors_it_holds:
+                if bag not in bag_held_in:
+                    bag_held_in[bag] = [super_bag]
+                else:
+                    bag_held_in[bag].append(super_bag)
+
+        # calculate number of color classes
+        s = 0
+        seen = set()
+        to_see = set(bag_held_in["shiny gold"])
+        while len(to_see) > 0:
+            bag = to_see.pop()
+            if bag not in seen:
+                s += 1
+                seen.add(bag)
+
+            if bag not in bag_held_in:
+                # bag isnt held in any other bag
+                continue
+
+            # add parents to the set we need to attend to
+            to_see = to_see.union(set(bag_held_in[bag]))
+
+        return s
 
 
-with open("input.txt", "r") as f:
+def part2():
+    Bag = str
+    with open("input.txt", "r") as f:
 
-    bag_held_in: Dict[Bag, List[Bag]] = dict()
+        bag_holds: Dict[Bag, List[Bag]] = dict()
 
-    for line in f.readlines():
-        line = line.replace("no other bags", "").replace(" bags", "").replace(" bag", "")
-        super_bag, b = line.split(" contain ")
+        for line in f.readlines():
+            line = (
+                line.replace("no other bags", "")
+                .replace(" bags", "")
+                .replace(" bag", "")
+                .replace(".\n", "")
+            )
+            super_bag, b = line.split(" contain ")
 
-        bags_it_holds = b.replace(".\n", "").split(", ")
-        bag_colors_it_holds = [
-            g[2:] for g in bags_it_holds if g != "no other bags"
-        ]  # first 2 chars are num and a space
+            bag_nums_it_holds = b.split(", ")
+            if bag_nums_it_holds != [""]:
+                bag_holds[super_bag] = [
+                    (int(g[:2]), g[2:])
+                    for g in bag_nums_it_holds
+                    if g != "no other bags"
+                ]  # first 2 chars are num and a space
 
-        for bag in bag_colors_it_holds:
-            if bag not in bag_held_in.keys():
-                bag_held_in[bag] = [super_bag]
-            else:
-                bag_held_in[bag].append(super_bag)
+        def f(n_b):
+            if n_b[1] not in bag_holds:
+                return n_b[0]
+            s = 0
+            for child_n_b in bag_holds[n_b[1]]:
+                s += n_b[0] * f(child_n_b)
+            return s + n_b[0]
 
-    s = 0
-    seen = set()
-    to_see = set(bag_held_in["shiny gold"])
-    while len(to_see) > 0:
-        bag = to_see.pop()
-        if bag not in seen:
-            s += 1
-            seen.add(bag)
-        if bag not in bag_held_in.keys():
-            continue
-        to_see = to_see.union(set(bag_held_in[bag]))
-    print(s)
+        return f((1, "shiny gold")) - 1
+
+
+print(part1())
+print(part2())
