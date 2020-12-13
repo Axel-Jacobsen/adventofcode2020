69c789da8f96c284842868f255e835ab3461048d
Axel-Jacobsen <axelnj44@gmail.com>
2020-12-12 20:52:42 -0800

    d11 p1

---
+++ day11/d11.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+
+
+import numpy as np
+
+
+EMPTY = "L"
+FLOOR = "."
+OCCUP = "#"
+
+
+def print_layout(layout):
+    for l in layout:
+        print("".join(l.tolist()))
+
+
+def chg(seat, neighbors):
+    if seat == EMPTY and all([n == EMPTY or n == "." for n in neighbors]):
+        return OCCUP
+    if seat == OCCUP and sum([int(n == OCCUP) for n in neighbors]) >= 4:
+        return EMPTY
+    return seat
+
+def get_neighbors(i,j,layout):
+    x_len, y_len = layout.shape
+    min_x, max_x = max(0, i - 1), min(i + 2, x_len)
+    min_y, max_y = max(0, j - 1), min(j + 2, y_len)
+    neighbors = layout[min_x:max_x, min_y:max_y].flatten().tolist()
+    neighbors.remove(layout[i, j])
+    return neighbors
+
+def step(prev_layout, next_layout):
+    x_len, y_len = prev_layout.shape
+    for i in range(x_len):
+        for j in range(y_len):
+            if prev_layout[i, j] == FLOOR:
+                continue
+            neighbors = get_neighbors(i,j,prev_layout)
+            next_layout[i, j] = chg(prev_layout[i, j], neighbors)
+
+
+with open("input.txt", "r") as f:
+    layout1 = np.asarray([list(row) for row in f.read().split("\n") if row != ""])
+    layout2 = np.asarray([row[:] for row in layout1])
+
+    step(layout1, layout2)  # init step
+
+    i = 1
+    while not np.array_equal(layout1, layout2):
+        if i % 2 == 0:
+            step(layout1, layout2)
+        else:
+            step(layout2, layout1)
+        i += 1
+
+    print((layout1 == OCCUP).astype(int).sum())
