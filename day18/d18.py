933085bc4a379cef7b85bf2b88821b440f3fa675
Axel-Jacobsen <axelnj44@gmail.com>
2020-12-18 15:24:23 -0800

    d18p2

--- day18/d18.py
+++ day18/d18.py
@@ -9,13 +9,13 @@ def get_exprs(fname):
     return raw_exprs
 
 
-def preprocess(s):
+def preprocess_p1(s):
     expr = []
     i = 0
     while i < len(s):
         if s[i] == "(":
             match = i + get_matching_closing(s[i:])
-            expr.append(preprocess(s[i + 1 : match]))
+            expr.append(preprocess_p1(s[i + 1 : match]))
             i = match + 1
         elif s[i] == "*":
             expr.append("*")
@@ -40,6 +40,40 @@ def evaluate(expr):
         elif isinstance(v, int):
             cval = apply(op, cval, v)
     return cval
+
+
+def preprocess_p2(s):
+    expr = []
+
+    i = 0
+    # first pass - clean up parentheses and integerify
+    while i < len(s):
+        if s[i] == "(":
+            match = i + get_matching_closing(s[i:])
+            expr.append(preprocess_p2(s[i + 1 : match]))
+            i = match + 1
+        elif s[i] == "*":
+            expr.append("*")
+            i += 1
+        elif s[i] == "+":
+            expr.append("+")
+            i += 1
+        else:
+            expr.append(int(s[i]))
+            i += 1
+
+    final_expr = []
+    i = 0
+    # second pass - collect plusses
+    while i < len(expr):
+        if expr[i] == "+":
+            prev_v = final_expr.pop()
+            final_expr.append([prev_v, "+", expr[i+1]])
+            i += 2
+        else:
+            final_expr.append(expr[i])
+            i += 1
+    return final_expr
 
 
 def get_matching_closing(s):
@@ -70,9 +104,19 @@ def p1(fname):
     s = 0
     raw_exprs = get_exprs(fname)
     for raw_exp in raw_exprs:
-        exp = preprocess(raw_exp)
+        exp = preprocess_p1(raw_exp)
+        s += evaluate(exp)
+    return s
+
+
+def p2(fname):
+    s = 0
+    raw_exprs = get_exprs(fname)
+    for raw_exp in raw_exprs:
+        exp = preprocess_p2(raw_exp)
         s += evaluate(exp)
     return s
 
 
 print(p1("input.txt"))
+print(p2("input.txt"))
