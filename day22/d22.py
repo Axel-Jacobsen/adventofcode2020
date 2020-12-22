00b380943754d68c3425c3ed8ddd2d510a0eee25
Axel-Jacobsen <axelnj44@gmail.com>
2020-12-22 11:45:41 -0800

    d22p1

---
+++ day22/d22.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+
+def read(fname):
+    with open(fname, "r") as f:
+        players = f.read().split("\n\n")
+
+    decks = []
+    for player in players:
+        decks.append(
+                [int(v) for v in player.split("\n")[1:] if v != ""]
+        )
+    return decks
+
+
+def play(deck1, deck2):
+    while len(deck1) > 0 and len(deck2) > 0:
+        d1_card = deck1.pop(0)
+        d2_card = deck2.pop(0)
+        if d1_card > d2_card:
+            deck1.extend([d1_card, d2_card])
+        elif d2_card > d1_card:
+            deck2.extend([d2_card, d1_card])
+        else:
+            raise RuntimeError("Tie! {d1_card} for {deck1} and {deck2}")
+
+    return deck1 if len(deck2) == 0 else deck2
+
+
+def calculate_score(deck):
+    return sum([i*v for i,v in enumerate(reversed(deck), start=1)])
+
+
+print(calculate_score(play(*read("input.txt"))))
+
