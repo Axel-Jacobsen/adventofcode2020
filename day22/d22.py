faa61a23e57a6a8f4150d02f46b093eb7292c7cf
Axel-Jacobsen <axelnj44@gmail.com>
2020-12-22 12:12:57 -0800

    its nicer now

--- day22/d22.py
+++ day22/d22.py
@@ -7,13 +7,11 @@ def read(fname):
 
     decks = []
     for player in players:
-        decks.append(
-                [int(v) for v in player.split("\n")[1:] if v != ""]
-        )
+        decks.append([int(v) for v in player.split("\n")[1:] if v != ""])
     return decks
 
 
-def play(deck1, deck2):
+def combat(deck1, deck2):
     while len(deck1) > 0 and len(deck2) > 0:
         d1_card = deck1.pop(0)
         d2_card = deck2.pop(0)
@@ -27,9 +25,40 @@ def play(deck1, deck2):
     return deck1 if len(deck2) == 0 else deck2
 
 
+def recursive_combat(deck1, deck2):
+    """return (winner, winner_deck) where winner is true if player 1 won"""
+    p1_seen_decks = set()
+    p2_seen_decks = set()
+
+    while len(deck1) > 0 and len(deck2) > 0:
+        # if either of the decks have been seen before, p1 wins
+        if tuple(deck1) in p1_seen_decks or tuple(deck2) in p2_seen_decks:
+            return True, deck1
+        else:
+            p1_seen_decks.add(tuple(deck1))
+            p2_seen_decks.add(tuple(deck2))
+
+        d1_card = deck1.pop(0)
+        d2_card = deck2.pop(0)
+
+        if d1_card <= len(deck1) and d2_card <= len(deck2):
+            p1_won, _ = recursive_combat(deck1[:d1_card], deck2[:d2_card])
+        else:
+            p1_won = d1_card > d2_card
+
+        if p1_won:
+            deck1.extend([d1_card, d2_card])
+        else:
+            deck2.extend([d2_card, d1_card])
+
+    return len(deck2) == 0, deck1 if len(deck2) == 0 else deck2
+
+
 def calculate_score(deck):
-    return sum([i*v for i,v in enumerate(reversed(deck), start=1)])
+    return sum([i * v for i, v in enumerate(reversed(deck), start=1)])
 
 
-print(calculate_score(play(*read("input.txt"))))
+print(calculate_score(combat(*read("input.txt"))))
 
+p1_won, deck = recursive_combat(*read("test_input.txt"))
+print(calculate_score(deck))
